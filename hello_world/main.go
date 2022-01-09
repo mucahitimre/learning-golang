9aefe78b51e51dc2c5e5879fd7b4bc05e7ebb691
Mücahit imre <mucahit.imre@gmail.com>
2022-01-09 23:23:51 +0300

    hello world folder added and all files move to hello world

--- hello_world/main.go
+++ hello_world/main.go
@@ -3,11 +3,18 @@ package main
 import (
 	"fmt"
 
-	"example.com/m/v2/mascot"
+	"example.com/m/v2/hello_world/mascot"
+)
+
+const (
+	max_size = 5
+	name     = "mico"
 )
 
 func main() {
 	message := "hello world"
 	println(message)
 	fmt.Println(mascot.BestMascot())
+	println(max_size)
+	println(name)
 }
