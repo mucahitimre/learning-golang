e79d5f01d7a81b2abf22fd3371912c39b949c06e
Mücahit imre <mucahit.imre@gmail.com>
2022-01-10 22:23:06 +0300

    string  aperations and folder editted the previous

--- 02_array_slices_map/main.go
+++ 02_array_slices_map/main.go
@@ -1,6 +1,6 @@
 package main
 
-import "example.com/m/v2/02_array_slices/examplepackage"
+import "example.com/m/v2/02_array_slices_map/examplepackage"
 
 func main() {
 	examplepackage.ExampleArray()
--- 03_string/main.go
+++ 03_string/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"strings"
 )
 
@@ -20,7 +21,42 @@ func main() {
 	}
 
 	var words = strings.Split(name, " ")
-	for _, v := range words {
-		fmt.Printf("%v | ", v)
+	for index, value := range words {
+		fmt.Printf("%v %d| \n", value, index)
+	}
+
+	count := GetDataWordCount()
+	list := make([]string, 0)
+	for i := 0; i < count; i++ {
+		rnd := rand.Intn(i + 1)
+		indexData := GetRandomName(rnd)
+		list = append(list, indexData)
+	}
+
+	last := list[len(list)-1]
+	for index, item := range list {
+		printed := "%d-%v -> "
+		if last == item {
+			printed = "%d-%v "
+		}
+
+		fmt.Printf(printed, index, item)
 	}
 }
+
+func GetRandomName(index int) string {
+	if index > GetDataWordCount() {
+		panic("index out of range")
+	}
+
+	dataList := strings.Split(data, " ")
+	var val = dataList[index]
+
+	return val
+}
+
+func GetDataWordCount() int {
+	return len(strings.Split(data, " "))
+}
+
+const data string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
